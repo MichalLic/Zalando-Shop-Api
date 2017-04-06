550f962145d2314cddefa5eb421b026ee110e44d
Michal <m14lickiewicz@gmail.com>
2017-04-06 19:05:53 +0200

    Set function to draw proper products, add comments to all functions.

--- src/js/script.js
+++ src/js/script.js
@@ -1,41 +1,77 @@
 var Zalando = {
     //variables
-
+    URL: 'https://api.zalando.com/articles',
+    $CATEGORY_LINK: $('.category-link'),
+    $PRODUCT_SECTION: $('.products-section'),
 
     //init
     init: function () {
-        Zalando.getProducts();
+        Zalando.getProducts(false);
+        Zalando.onInitTrueFunction(Zalando.$CATEGORY_LINK);
     },
+
     //function
-    getProducts: function () {
+    /**
+     * get products from proper url
+     * @param isFilter
+     * @param endpoint
+     */
+    getProducts: function (isFilter, endpoint) {
+        var filters = (isFilter == true) ? endpoint : "";
         $.ajax({
-            url: 'https://api.zalando.com/articles',
+            url: Zalando.URL + filters,
             method: 'get',
             dataType: 'JSON',
             success: function (response) {
-                console.log(response);
+                $('.product-link').remove();
                 Zalando.getSingleElement(response);
             },
             error: function () {
-                console.log('upsss')
+                alert ("Error getting data")
             }
         });
     },
 
+    /**
+     * init function when is true (have extra endpoint)
+     * @param btn
+     */
+    onInitTrueFunction: function (btn) {
+        btn.on('click', function (e) {
+            e.preventDefault();
+            Zalando.getProducts(true, Zalando.returnFilteredDataUrl(this));
+        })
+    },
+
+    /**
+     * create proper endpoint by 'param' and 'dataset' functions
+     * @param btn
+     * @returns {string}
+     */
+    returnFilteredDataUrl: function (btn) {
+        return  '?' + $.param(btn.dataset);
+    },
+
+    /**
+     * get single product after map function
+     * @param data
+     */
     getSingleElement: function (data) {
         $.map(data.content, function (product) {
             Zalando.drawMustache(product);
         });
     },
 
+    /**
+     * draw products on page content by mustache
+     * @param data
+     */
     drawMustache: function (data) {
         var template = $('#template').html();
         Mustache.parse(template);
         var rendered = Mustache.render(template, data);
-        $('.products-section').append(rendered);
+        Zalando.$PRODUCT_SECTION.append(rendered);
     }
-
-
 };
 
 $(document).ready(function () {
